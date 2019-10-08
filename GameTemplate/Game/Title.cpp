b918c2b474c0fcff1eaca9a6427bc5a5a2fce105
iwamoto takuya <kbc18b04@stu.kawahara.ac.jp>
2019-10-08 16:08:38 +0900

    Titleクラス追加

---
+++ GameTemplate/Game/Title.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "Title.h"
+#include "Game.h"
+
+Title::Title()
+{
+}
+
+
+Title::~Title()
+{
+	DeleteGO(m_spriteModelRender);
+}
+
+bool Title::Start() 
+{
+	m_spriteModelRender = NewGO<prefab::CSpriteRender>(0);
+	m_spriteModelRender->Init(L"sprite/title.dds",1300.0f, 1000.0f);
+	return true;
+}
+
+void Title::Update()
+{
+	if (Pad(0).IsTrigger(enButtonA))
+	{
+		NewGO<Game>(0);
+		DeleteGO(this);
+	}
+
+}
