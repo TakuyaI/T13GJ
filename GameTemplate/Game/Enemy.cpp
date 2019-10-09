17b9e4c6c1a784bff37afe5e0c2fa6226a4a513e
kijima <1>
2019-10-09 17:10:05 +0900

    敵モデルの回転と、移動速度を変えました。

--- GameTemplate/Game/Enemy.cpp
+++ GameTemplate/Game/Enemy.cpp
@@ -11,7 +11,6 @@ Enemy::Enemy()
 Enemy::~Enemy()
 {
 	DeleteGO(m_skinModelRender);
-
 }
 
 bool Enemy::Start()
@@ -19,9 +18,12 @@ bool Enemy::Start()
 	//スキンモデルレンダラーの作成
 	m_skinModelRender = NewGO<prefab::CSkinModelRender>(0);
 	m_skinModelRender->Init(L"modelData/r2.cmo");
+	
+	/*m_rotation.SetRotationDeg(CVector3::AxisY, 180.0f);
+
 	m_rotation.SetRotationDeg(CVector3::AxisX, 90.0f);
-
-
+	m_skinModelRender->SetRotation(m_rotation);
+*/
 	//モデルの大きさ
 	CVector3 scale;
 	scale.x = 2.0f;
@@ -32,7 +34,7 @@ bool Enemy::Start()
 
 	m_position.x = 500.0f;
 	m_position.y = 15.0f;
-	m_position.z = 300.0f;
+	m_position.z = 500.0f;
 	enechara.Init(
 		20.0f,
 		70.0f,
@@ -44,7 +46,7 @@ bool Enemy::Start()
 }
 void Enemy::Move()
 {
-	//左右移動	
+	//左右移動
 	float pposix= FindGO<Player>("player")->m_position.x;
 	if (pposix > m_position.x) {
 		m_position.x += 5.0f;
@@ -54,10 +56,10 @@ void Enemy::Move()
 	}
 	//奥行方向
 	float pposiz = FindGO<Player>("player")->m_position.z;
-	if (pposiz > m_position.z+300.0f) {
+	if (pposiz > m_position.z-500.0f) {
 		m_position.z += 5.0f;
 	}
-	if (pposiz < m_position.z-300.0f) {
+	if (pposiz <= m_position.z-500.0f) {
 		m_position.z -= 5.0f;
 	}
 
@@ -71,6 +73,7 @@ void Enemy::Attack()
 	if (diff.Length() < 300.0f) {
 		attack = 1;
 	}
+	float pposix = FindGO<Player>("player")->m_position.x;
 	if (attack == 1); {
 		attack = 0;
 	}
