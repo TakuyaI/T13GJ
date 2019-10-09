c993c830c44dbb8ad5c735141242ec59a11a223a
iwamoto takuya <kbc18b04@stu.kawahara.ac.jp>
2019-10-09 10:59:20 +0900

    カメラ機能追加

---
+++ GameTemplate/Game/GameCamera.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include "GameCamera.h"
+
+
+GameCamera::GameCamera()
+{
+}
+
+
+GameCamera::~GameCamera()
+{
+}
+
+bool GameCamera::Start()
+{
+	//ƒJƒƒ‰‚ğİ’èB
+	MainCamera().SetNear(10.0f);
+	MainCamera().SetFar(10000.0f);
+	m_toCameraPos = { 0.0f, 70.0f, 100.0f };
+	m_player = FindGO<Player>("player");
+	return true;
+}
+
+void GameCamera::Update()
+{
+	m_target = m_player->m_position;
+	m_target.y += 50.0f;
+
+	m_position = m_target + m_toCameraPos;
+
+	MainCamera().SetTarget(m_target);
+	MainCamera().SetPosition(m_position);
+	MainCamera().Update();
+}
---
+++ GameTemplate/Game/GameCamera.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Player.h"
+class GameCamera : public IGameObject
+{
+public:
+	GameCamera();
+	~GameCamera();
+
+	bool Start();
+	void Update();
+
+	Player* m_player;
+
+	CVector3 m_position = CVector3::Zero;
+	CVector3 m_target = CVector3::Zero;
+	CVector3 m_toCameraPos = CVector3::Zero;
+};
+
