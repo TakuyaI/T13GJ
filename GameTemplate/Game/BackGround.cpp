a829ea61436c66f4840b45e18ec85db38735694b
iwamoto takuya <kbc18b04@stu.kawahara.ac.jp>
2019-10-09 09:59:59 +0900

    backgroundオブジェクト追加

--- GameTemplate/Game/BackGround.cpp
+++ GameTemplate/Game/BackGround.cpp
@@ -10,3 +10,15 @@ BackGround::BackGround()
 BackGround::~BackGround()
 {
 }
+
+bool BackGround::Start()
+{
+	m_skinModelRender = NewGO<prefab::CSkinModelRender>(0);
+	m_skinModelRender->Init(L"modelData/background.cmo");
+	return true;
+}
+
+void BackGround::Update()
+{
+
+}
