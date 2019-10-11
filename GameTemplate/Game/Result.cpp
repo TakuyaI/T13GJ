a260443ae924ca669ddc7210fa3d153c649e48a0
nagano <t>
2019-10-11 11:50:51 +0900

    自分と敵のスコアを画面外から表示させた。

--- GameTemplate/Game/Result.cpp
+++ GameTemplate/Game/Result.cpp
@@ -23,14 +23,15 @@ bool Result::Start()
 	spriteRender->SetPosition(position);
 	m_fontRender = NewGO<prefab::CFontRender>(0);
 	m_fontRender->SetText(L"スコア　P1");
-	m_position={ 700.0f,0.0f };
+	m_position={ 300.0f,0.0f };
 	m_fontRender->SetPosition(m_position);
 	fontRender = NewGO<prefab::CFontRender>(0);
 	fontRender->SetText(L"スコア　P2");
-	m_2position = { 700.0f,-200.0f };
+	m_2position = { 300.0f,-170.0f };
 	fontRender->SetPosition(m_2position);
 	m_Score = FindGO<Game>("game");
 	m_score = m_Score->m_score;
+	m_enescore = m_Score->m_enescore;
 	return true;
 
 }
@@ -42,7 +43,17 @@ void Result::PostRender(CRenderContext& rc)
 	m_font.Begin(rc);
 	m_font.Draw(
 		text,
-		{ 350.0f, 0.0f },
+		heikou,
+		{ 0.0f, 0.0f, 0.0f, 1.0f },
+		0.0f,
+		1.8f,
+		{ 0.0f, 1.0f }
+	);
+
+	swprintf_s(text, L"%d", m_enescore);
+	m_font.Draw(
+		text,
+		tousoku,
 		{ 0.0f, 0.0f, 0.0f, 1.0f },
 		0.0f,
 		1.8f,
@@ -53,17 +64,17 @@ void Result::PostRender(CRenderContext& rc)
 
 void Result::Update()
 {
-	if (m_position.x <= 800.0f){
-		m_position.x -= 10.0f;
+	if (heikou.x <= 800.0f){
+		heikou.x -= 10.0f;
 	}
-	if (m_position.x <= 300.0f) {
-		m_position.x = 300.0f;
+	if (heikou.x <= 360.0f) {
+		heikou.x = 360.0f;
 	}
-	if (m_2position.x <= 800.0f) {
-		m_2position.x -= 10.0f;
+	if (tousoku.x <= 800.0f) {
+		tousoku.x -= 10.0f;
 	}
-	if (m_2position.x <= 300.0f) {
-		m_2position.x = 300.0f;
+	if (tousoku.x <= 360.0f) {
+		tousoku.x = 360.0f;
 	}
 	/*else if () {
 		//2Dが上から落ちてくる処理。
--- GameTemplate/Game/Result.h
+++ GameTemplate/Game/Result.h
@@ -12,6 +12,7 @@ public:
 	CFont m_font;
 	int Score = 0;
 	Game* m_Score;
+
 	prefab::CSpriteRender* m_spriteRender = nullptr;//リザルト画面
 	prefab::CSpriteRender* spriteRender = nullptr;//ゲームクリアのやつ
 	CVector3 position = CVector3::Zero;//ゲームクリアのポジション
@@ -20,6 +21,9 @@ public:
 	CVector2 m_position = CVector2::Zero;
 	CVector2 m_2position = CVector2::Zero;
 	int m_score = 0;
+	int m_enescore = 0;
 	int m_flag = 0;
+	CVector2 heikou = { 650.0f, 0.0f };
+	CVector2 tousoku = { 650.0f,-180.0f };
 };
 
